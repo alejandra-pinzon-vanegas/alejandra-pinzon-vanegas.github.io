0d408519036462677af2a6b1ac57e816b4e56c38
alejandra-pinzon-vanegas <73900519+alejandra-pinzon-vanegas@users.noreply.github.com>
2020-11-08 23:26:03 -0500

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,6 @@
+var bntMenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+
+bntMenu.addEventListener('click', function(){
+    nav.classList.toggle('mostrar');                    
+})
